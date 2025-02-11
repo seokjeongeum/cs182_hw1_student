Fix batch normalization calculation by using square root for variance

deeplearning/layers.py
@@ -186,7 +186,7 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # and shift the normalized data using gamma and beta. Store the result in   #
         # the out variable.                                                         #
         #############################################################################
-        out = (x - running_mean) / (running_var + eps) * gamma + beta
+        out = (x - running_mean) / np.sqrt(running_var + eps) * gamma + beta
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################